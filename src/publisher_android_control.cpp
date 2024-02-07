b77f54e79ecf919899af5d1a636411865cb1b473
rgabbai <rony.gabbai@gmail.com>
2024-02-07 13:30:53 +0200

    updated code

--- src/publisher_android_control.cpp
+++ src/publisher_android_control.cpp
@@ -42,6 +42,12 @@ using namespace std::chrono_literals;
 int main(int argc, char *argv[]) {
 
 
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("android_publisher");
+  auto message = std_msgs::msg::String();
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  publisher_ = node->create_publisher<std_msgs::msg::String>("gui", 10);
+
   int listening = socket(AF_INET, SOCK_STREAM, 0);
 
   if (listening == -1)
@@ -50,13 +56,23 @@ int main(int argc, char *argv[]) {
       return -1;
   }
 
+  int opt = 1;
+  // Set SO_REUSEADDR on a socket to true (1): This allow connecting also to connect during TIME_WAIT
+  if (setsockopt(listening, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+    cerr << "Can't set socket options! Quitting" << endl;
+    return -1;
+  }
+
   // Bind the ip address and port to a socket
   sockaddr_in hint;
   hint.sin_family = AF_INET;
   hint.sin_port = htons(6000);
-  inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+  inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // for any address
 
-  bind(listening, (sockaddr*)&hint, sizeof(hint));
+  if (bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1) {
+    RCLCPP_INFO(node->get_logger(), "Can't bind");
+    return -2;
+  };
 
   // Tell Winsock the socket is for listening
   listen(listening, SOMAXCONN);
@@ -67,6 +83,42 @@ int main(int argc, char *argv[]) {
 
   int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
 
+  if (clientSocket == -1) {
+    cerr << "Can't accept connection" << endl;
+    return -1;
+  }
+
+
+  // Convert client's IP address from binary to text
+  char clientIP[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &client.sin_addr, clientIP, INET_ADDRSTRLEN);
+
+  // Check if the client's IP matches the desired IP
+  if (strcmp(clientIP, "10.242.57.245") != 0) {
+    // IP address does not match, close the connection
+    close(clientSocket);
+    cout << "Connection from " << clientIP << " rejected." << endl;
+  } else {
+    // IP address matches, proceed with handling the connection
+    cout << "Connection from Pixel 7 IP:" << clientIP << " accepted." << endl;
+    // Handle the connection, e.g., by reading or writing data
+  }  
+
+  // Connection has been established, now send a message to the client
+  const char* welcomeMsg = "Robot server connected";
+  int sendResult = send(clientSocket, welcomeMsg, strlen(welcomeMsg) + 1, 0);
+  if (sendResult == -1) {
+    cerr << "Failed to send message to client" << endl;
+    // Handle error, e.g., by closing the socket
+  } else {
+    cout << "Message sent to client: " << welcomeMsg << endl;
+  }
+
+
+
+
+
+
   char host[NI_MAXHOST];      // Client's remote name
   char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
 
@@ -75,17 +127,20 @@ int main(int argc, char *argv[]) {
   // Close listening socket
   close(listening);
  
+
   // While loop: accept and echo message back to client
   char buf[4096];
 
-  rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("minimal_publisher");
-  auto message = std_msgs::msg::String();
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  publisher_ = node->create_publisher<std_msgs::msg::String>("topic", 10);
+ // Try to convert the IP to a human-readable form, otherwise use inet_ntoa
+  if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+        cout << "Host connected: " << host << endl;
+  } else {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << "Host connected: " << inet_ntoa(client.sin_addr) << endl;
+        RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", inet_ntoa(client.sin_addr));
 
-
+  }
 
 while (true)
     {
@@ -106,6 +161,7 @@ while (true)
             break;
         }
         //RCLCPP_INFO(node->get_logger(), buf);
+        buf[strcspn(buf, "\n")] = 0;
         message.data = buf;
         RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
         publisher_->publish(message);
